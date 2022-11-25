12a0c698df1377c04946f719185450ec464700f9
hopefyjamshed <103117529+hopefyjamshed@users.noreply.github.com>
2022-11-26 03:44:36 +0600

    product get by category added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const cors = require('cors');
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000
 require('dotenv').config()
@@ -13,6 +14,60 @@ app.use(express.json())
 app.get('/', (req, res) => {
     res.send('monitor dokan server is Running successfully')
 })
+
+
+// mongodb connection 
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jle6tre.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const productsCollection = client.db('monitorDokan').collection('products')
+
+
+
+        app.get('/products', async (req, res) => {
+            const query = {}
+            const result = await productsCollection.find(query).toArray()
+            res.send(result)
+
+        })
+        app.get('/products', async (req, res) => {
+            const filter = {}
+            const options = { Upsert: true }
+            const updatedDoc = {
+                $set: {
+                    products: [
+                        {
+                            name: 'ami'
+                        }
+                    ]
+                }
+            }
+            const result = await productsCollection.updateMany(filter, updatedDoc, options)
+            res.send(result)
+        })
+
+        // category 
+
+        app.get('/products/:category', async (req, res) => {
+            const category = req.params.category
+            const query = { categoryName: category }
+            const result = await productsCollection.find(query).toArray()
+            res.send(result)
+        })
+
+
+    }
+    finally { }
+
+
+}
+run().catch(err => console.error(err))
+
 
 app.listen(port, () => {
     console.log(`server is running on port ${port}`)
